4900c604a3242e59d36c620d327b80f756a5ae47
Andi Firmansyah <andfirsyah58@gmail.com>
2024-10-30 20:10:37 +0700

    selesai, tapi tidak responsif, hanya bisa dilayar creator aja

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,15 @@ module.exports = {
     extend: {
       backgroundImage: {
         'hero': "url('../img/hero.png')",
+        'aboutUs': "url('../img/about-us-2.jpg')",
+        'sideAboutUs': "url('../img/side-about-us.jpg')",
         'bestService': "url('../img/world_map_PNG28.png')",
         'destination': "url('../img/world_map_PNG28_2.png')"
       },
       fontFamily: {
         OpenSans: 'OpenSans',
-        Poppins: 'Poppins'
+        Poppins: 'Poppins',
+        Montserrat: 'Montserrat'
       },
       colors: {
         second: "#005853",
@@ -24,7 +27,14 @@ module.exports = {
         font1: '#5E6282',
         font2: '#84829A',
         font3: '#8A79DF',
-        abuCus: '#F5F5F5'
+        hijauCus: '#9FFFEC',
+        abuCus: '#F5F5F5',
+        insta1: '#4f5bd5',
+        insta2: '#962fbf',
+        insta3: '#d62976',
+        insta4: '#fa7e1e',
+        insta5: '#feda75',
+        facebook: '#3b5998'
       }
     },
   },
